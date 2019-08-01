124457644b565c5bb6a886c2568ddf4498f13f6c
jehanzaib <jehanzaib_awan@hotmail.com>
2019-08-01 06:26:38 +0400

    set OAuth client id and secret to up the api limit

--- src/api/github/config.js
+++ src/api/github/config.js
@@ -1,1 +1,3 @@
 export const API_ADDRESS = "https://api.github.com";
+export const API_CLIENT_ID = "286266860121928c4467";
+export const API_CLIENT_SECRET = "dcf49620fc76371f5f56993aface3d972f70a97f";
--- src/api/github/index.js
+++ src/api/github/index.js
@@ -1,10 +1,13 @@
-import { API_ADDRESS } from "./config";
+import { API_ADDRESS, API_CLIENT_ID, API_CLIENT_SECRET } from "./config";
 
 export const getUserGists = async username => {
   try {
-    const response = await fetch(`${API_ADDRESS}/users/${username}/gists`, {
-      method: "get"
-    });
+    const response = await fetch(
+      `${API_ADDRESS}/users/${username}/gists?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`,
+      {
+        method: "get"
+      }
+    );
 
     return await response.json();
   } catch (ex) {
@@ -15,9 +18,12 @@ export const getUserGists = async username => {
 
 export const getGistForks = async fork_url => {
   try {
-    const response = await fetch(fork_url, {
-      method: "get"
-    });
+    const response = await fetch(
+      `${fork_url}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`,
+      {
+        method: "get"
+      }
+    );
 
     return await response.json();
   } catch (ex) {
